2eead0acbe2c42db9e7bd4d28920b4b4254cb26c
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-10-10 12:02:02 +0000

    yanchangyou->org.ghcc-demo.ware.helloworld-alpha-0.1-0.1-0.1 1, 添加本地依赖jar测试, 本地依赖jar可以自己构建classloader加载, 试验成功

--- trunk/groups/org/ghcc/ghcc-0/ghcc-0.1/products/demo/ware/helloworld/helloworld-0/helloworld-0.1/projects/alpha/alpha-0/alpha-0.1/org.ghcc-demo.ware.helloworld-alpha-0.1-0.1-0.1/java-code/org/ghcc/demo/ware/helloworld/Helloworld.java
+++ trunk/groups/org/ghcc/ghcc-0/ghcc-0.1/products/demo/ware/helloworld/helloworld-0/helloworld-0.1/projects/alpha/alpha-0/alpha-0.1/org.ghcc-demo.ware.helloworld-alpha-0.1-0.1-0.1/java-code/org/ghcc/demo/ware/helloworld/Helloworld.java
@@ -2,6 +2,7 @@ package org.ghcc.demo.ware.helloworld;
 
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.ghcc.toft.ware.core.Driver;
 
 
@@ -9,6 +10,7 @@ public class Helloworld implements Driver {
 
 	public void drive(Map<String, Object> context) throws Exception {
 		System.out.println("hello world!");
+		System.out.println(StringUtils.capitalize("abc"));
 	}
 
 	public void dealWareException(Exception WareException) throws Exception {
--- trunk/groups/org/ghcc/ghcc-0/ghcc-0.1/products/demo/ware/helloworld/helloworld-0/helloworld-0.1/projects/alpha/alpha-0/alpha-0.1/org.ghcc-demo.ware.helloworld-alpha-0.1-0.1-0.1/java-code/org/ghcc/demo/ware/helloworld/HelloworldWare.java
+++ trunk/groups/org/ghcc/ghcc-0/ghcc-0.1/products/demo/ware/helloworld/helloworld-0/helloworld-0.1/projects/alpha/alpha-0/alpha-0.1/org.ghcc-demo.ware.helloworld-alpha-0.1-0.1-0.1/java-code/org/ghcc/demo/ware/helloworld/HelloworldWare.java
@@ -1,5 +1,11 @@
 package org.ghcc.demo.ware.helloworld;
 
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.dom4j.DocumentHelper;
@@ -7,6 +13,7 @@ import org.dom4j.Element;
 import org.ghcc.toft.ware.core.Driver;
 import org.ghcc.toft.ware.core.KeywordsUtil;
 import org.ghcc.toft.ware.core.Ware;
+import org.ghcc.toft.ware.core.WareEntity;
 
 
 public class HelloworldWare implements Ware {
@@ -22,7 +29,27 @@ public class HelloworldWare implements Ware {
 		Element element = DocumentHelper.parseText(xmlElement).getRootElement(); 
 		String className = KeywordsUtil.getKeyWordsClass(this.getClass(), element);
 		System.out.println(className);
-		Driver driver = (Driver) Class.forName(className).newInstance();
+		
+		ClassLoader current = Thread.currentThread().getContextClassLoader();
+		URI uri = (URI) context.get("ware-uri");
+		
+		String uriString = uri.toString();
+		String root = uriString.substring(0, uriString.lastIndexOf('/'));
+		String id = uriString.substring(uriString.lastIndexOf('/')+1);
+//		URL projectURL = new URL(root + "/" + getWareProjectPath(id));
+		
+		
+		List<URL> classPathURLList = new ArrayList<URL>();
+		classPathURLList.add(new URL("http://localhost:9999/org.ghcc-demo.ware.helloworld-alpha-0.1-0.1-0.1/depend/java-lib/commons-lang-2.4.jar"));
+		classPathURLList.addAll(Arrays.asList(WareEntity.getClassLoaderURLs(uri)));
+		URL[] urls = classPathURLList.toArray(new URL[]{});
+		
+		URLClassLoader loader = new URLClassLoader(urls, current);
+		
+		Thread.currentThread().setContextClassLoader(loader);
+		Driver driver = (Driver) loader.loadClass(className).newInstance();
+
+		System.out.println("driver : " + driver);
 		driver.drive(context);
 	}
 	public void dealExecuteException(Exception wareException) throws Exception {
