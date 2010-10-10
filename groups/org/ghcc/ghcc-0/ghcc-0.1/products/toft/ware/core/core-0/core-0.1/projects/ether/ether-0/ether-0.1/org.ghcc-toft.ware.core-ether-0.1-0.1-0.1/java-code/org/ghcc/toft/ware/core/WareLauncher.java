3b6fbec8a91c581125a0ea413330dd7bd9a3c626
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-10-10 12:00:41 +0000

    yanchangyou->org.ghcc-toft.ware.core-ether-0.1-0.1-0.1 1, 添加本地依赖jar包处理, 把当前的class查找的uri传递到下面类, 然后处理, 后面构造完整的loader, 此loader能够加载, 这些依赖的jar, 这样就不需要copy到客户端了

--- groups/org/ghcc/ghcc-0/ghcc-0.1/products/toft/ware/core/core-0/core-0.1/projects/ether/ether-0/ether-0.1/org.ghcc-toft.ware.core-ether-0.1-0.1-0.1/java-code/org/ghcc/toft/ware/core/WareLauncher.java
+++ groups/org/ghcc/ghcc-0/ghcc-0.1/products/toft/ware/core/core-0/core-0.1/projects/ether/ether-0/ether-0.1/org.ghcc-toft.ware.core-ether-0.1-0.1-0.1/java-code/org/ghcc/toft/ware/core/WareLauncher.java
@@ -22,7 +22,12 @@ public class WareLauncher {
 		WareEntity ware = new WareEntity(uri);
 		
 		Map<String,Object> context = new HashMap<String,Object>();
+		ClassLoader currentClassLoader = ware.getClassLoader();
 		
+		context.put("this-class-loader", currentClassLoader);
+		context.put("ware-uri", ware.getUri());
+		
+//		Thread.currentThread().setContextClassLoader(currentClassLoader);
 		ware.getWare().execute(wareDocument.getRootElement().asXML(), context);
 	}
 }
