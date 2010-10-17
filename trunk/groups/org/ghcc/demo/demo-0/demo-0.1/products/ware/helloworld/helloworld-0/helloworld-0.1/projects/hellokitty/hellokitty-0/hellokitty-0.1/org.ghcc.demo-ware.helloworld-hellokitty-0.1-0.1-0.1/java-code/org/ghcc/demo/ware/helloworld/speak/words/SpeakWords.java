00843d5de7c6e0a6b03344777e69763e260f94f1
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-10-17 17:07:11 +0000

    yanchangyou->org.ghcc.demo-app.helloworld-hellokitty-0.1-0.1-0.1 1, 去掉 commons测试类

--- trunk/groups/org/ghcc/demo/demo-0/demo-0.1/products/ware/helloworld/helloworld-0/helloworld-0.1/projects/hellokitty/hellokitty-0/hellokitty-0.1/org.ghcc.demo-ware.helloworld-hellokitty-0.1-0.1-0.1/java-code/org/ghcc/demo/ware/helloworld/speak/words/SpeakWords.java
+++ trunk/groups/org/ghcc/demo/demo-0/demo-0.1/products/ware/helloworld/helloworld-0/helloworld-0.1/projects/hellokitty/hellokitty-0/hellokitty-0.1/org.ghcc.demo-ware.helloworld-hellokitty-0.1-0.1-0.1/java-code/org/ghcc/demo/ware/helloworld/speak/words/SpeakWords.java
@@ -1,6 +1,5 @@
 package org.ghcc.demo.ware.helloworld.speak.words;
 
-import org.apache.commons.lang.StringUtils;
 import org.ghcc.toft.ware.core.impl.ether.abstracts.matrix.EtherAbstractFunctionEntity;
 import org.ghcc.toft.ware.core.impl.ether.interfaces.matrix.EtherFunctionNode;
 
@@ -9,7 +8,6 @@ public class SpeakWords extends EtherAbstractFunctionEntity {
 	public void drive(EtherFunctionNode node) throws Exception {
 //		System.out.println("to SpeakWords ");
 		System.out.println(node.getDom4jElement().getText());
-		System.out.println("StringUtils.capitalize(\"abc\") = " + StringUtils.capitalize("abc"));
 	}
 
 }
