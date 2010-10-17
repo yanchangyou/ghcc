254caff37628eed11b200192eaa9bd79636844b7
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-10-17 07:27:16 +0000

    yanchangyou->org.ghcc.toft-ware.manager-tinder-0.1-0.1-0.1 1, 新建 项目, 用于创建ware项目 2，使用WareID代替原来的Ware

---
+++ trunk/groups/org/ghcc/toft/toft-0/toft-0.1/products/ware/manager/manager-0/manager-0.1/projects/tinder/tinder-0/tinder-0.1/org.ghcc.toft-ware.manager-tinder-0.1-0.1-0.1/java-code/org/ghcc/toft/ware/manager/WareHandler.java
@@ -0,0 +1,6 @@
+package org.ghcc.toft.ware.manager;
+
+public interface WareHandler {
+
+	public void handle(WareID ware) throws Exception ;
+}
---
+++ trunk/groups/org/ghcc/toft/toft-0/toft-0.1/products/ware/manager/manager-0/manager-0.1/projects/tinder/tinder-0/tinder-0.1/org.ghcc.toft-ware.manager-tinder-0.1-0.1-0.1/java-code/org/ghcc/toft/ware/manager/drop/ware/project/DropWareProject.java
@@ -0,0 +1,65 @@
+package org.ghcc.toft.ware.manager.drop.ware.project;
+
+import java.io.File;
+
+import org.ghcc.toft.ware.manager.WareID;
+import org.ghcc.toft.ware.manager.WareHandler;
+
+public class DropWareProject implements WareHandler {
+	/**
+	 * 删除项目文件夹:<br>
+	 * --{project-root}<br>
+	 * ---------------java-code                   存放java代码<br>
+	 * ---------------depend                      项目依赖的东西<br>
+	 * ---------------------java-lib              依赖的jar包<br>
+	 * ---------------product                     产品目录<br>
+	 * ----------------------java-classes         classes产品<br>
+	 * ----------------------java-lib             jar包产品<br>
+	 * 
+	 * @param wareProjectDirectoryPath
+	 */
+	public void dropWareProject(WareID ware) {
+		File wareProjectDirectory = ware.getProjectDirectory();
+		File groupsDirectory = ware.getGroupsDirectory();
+		File rootEmptyFile = getRootEmptyParentFile(groupsDirectory, wareProjectDirectory);
+		deleteDictory(rootEmptyFile);
+	}
+	/**
+	 * 删除指定目录下文件, 同时也删除空的父目录, 指定根目录<br>
+	 * 为了解决创建 ware 项目时创建大量空目录, 而不能删除
+	 * @param root
+	 * @param path
+	 */
+	public static File getRootEmptyParentFile(File root, File file) {
+		File parent = file.getParentFile();
+		if(parent != null && 
+				parent.list() != null && 
+				parent.list().length == 1 && //只有自身文件夹
+				parent.getAbsolutePath().indexOf(root.getAbsolutePath()) > -1) { // 不能到达根目录
+			return getRootEmptyParentFile(root, parent);
+		}
+		return file;
+	}
+	
+	public static void deleteDictory(File dir) {
+		if(dir.isDirectory()) {
+			for(File file : dir.listFiles()) {
+				deleteDictory(file);
+			}
+		}
+		boolean isOk = dir.delete();
+		try {
+			Thread.sleep(100);  //可能速度快了, 不能递归删除, 暂停减慢速度
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("删除文件[" + isOk + "]!  　路径:" + dir.getAbsolutePath());
+	}
+	
+	public static void main(String[] args) {
+		deleteDictory(new File("L:/test/ghcc/groups/org/"));
+	}
+	public void handle(WareID ware) throws Exception {
+		dropWareProject(ware);
+	}
+}
