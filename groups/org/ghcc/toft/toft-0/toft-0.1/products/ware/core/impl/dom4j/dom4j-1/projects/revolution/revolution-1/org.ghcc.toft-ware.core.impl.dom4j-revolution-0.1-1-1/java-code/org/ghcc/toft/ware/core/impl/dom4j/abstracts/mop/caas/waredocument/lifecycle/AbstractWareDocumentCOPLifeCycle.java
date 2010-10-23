bd211645594147cb6a08d9cbd45f164abdefe291
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-10-23 17:20:05 +0000

    yanchangyou->org.ghcc.toft-ware.core.impl.dom4j-revolution-0.1-1-1 1, 添加 waredocument部分的简单代码测试

---
+++ groups/org/ghcc/toft/toft-0/toft-0.1/products/ware/core/impl/dom4j/dom4j-1/projects/revolution/revolution-1/org.ghcc.toft-ware.core.impl.dom4j-revolution-0.1-1-1/java-code/org/ghcc/toft/ware/core/impl/dom4j/abstracts/mop/caas/waredocument/lifecycle/AbstractWareDocumentCOPLifeCycle.java
@@ -0,0 +1,23 @@
+/*
+ * CopyCenter 2010 GHCC.ORG, all rights freed.
+ */
+
+package org.ghcc.toft.ware.core.impl.dom4j.abstracts.mop.caas.waredocument.lifecycle;
+
+import org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.lifecycle.WareDocumentCOPLifeCycle;
+import org.ghcc.toft.ware.core.impl.dom4j.abstracts.mop.caas.waredocument.AbstractWareDocumentCOP;
+
+
+/**
+ * AbstractWareDocumentCOPLifeCycle
+ * 
+ * @author code machine
+ * @author yanchangyou
+ * @date 2010-10-23 23:38:13
+ * @version 0.1-1-1
+ *
+ */
+
+
+public class AbstractWareDocumentCOPLifeCycle extends AbstractWareDocumentCOP implements WareDocumentCOPLifeCycle {
+}
---
+++ groups/org/ghcc/toft/toft-0/toft-0.1/products/ware/core/impl/dom4j/dom4j-1/projects/revolution/revolution-1/org.ghcc.toft-ware.core.impl.dom4j-revolution-0.1-1-1/java-code/org/ghcc/toft/ware/core/impl/dom4j/abstracts/mop/caas/waredocument/lifecycle/AbstractWareDocumentLoader.java
@@ -0,0 +1,91 @@
+/*
+ * CopyCenter 2010 GHCC.ORG, all rights freed.
+ */
+
+package org.ghcc.toft.ware.core.impl.dom4j.abstracts.mop.caas.waredocument.lifecycle;
+
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+import org.ghcc.toft.ware.core.design.interfaces.cop.define.ID;
+import org.ghcc.toft.ware.core.design.interfaces.cop.define.PathInfo;
+import org.ghcc.toft.ware.core.design.interfaces.cop.exception.LoadException;
+import org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.define.WareDocumentID;
+import org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.define.WareDocumentPathInfo;
+import org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.exception.WareDocumentLoadException;
+import org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.lifecycle.WareDocumentLoader;
+import org.ghcc.toft.ware.core.impl.dom4j.abstracts.mop.caas.waredocument.AbstractWareDocumentEntity;
+
+
+/**
+ * AbstractWareDocumentLoader
+ * 
+ * @author code machine
+ * @author yanchangyou
+ * @date 2010-10-23 23:38:13
+ * @version 0.1-1-1
+ *
+ */
+
+
+public class AbstractWareDocumentLoader extends AbstractWareDocumentCOPLifeCycle implements WareDocumentLoader {
+
+	/* (non-Javadoc)
+	 * @see org.ghcc.toft.ware.core.design.interfaces.cop.lifecycle.Loader#load(org.ghcc.toft.ware.core.design.interfaces.cop.define.ID)
+	 */
+	public AbstractWareDocumentEntity load(ID id) throws LoadException {
+		return load((WareDocumentID)id);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ghcc.toft.ware.core.design.interfaces.cop.lifecycle.Loader#load(org.ghcc.toft.ware.core.design.interfaces.cop.define.PathInfo, org.ghcc.toft.ware.core.design.interfaces.cop.define.ID)
+	 */
+	public AbstractWareDocumentEntity load(PathInfo pathInfo, ID id) throws LoadException {
+		return load((WareDocumentPathInfo)pathInfo, (WareDocumentID)id);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.lifecycle.WareDocumentLoader#load(org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.define.WareDocumentID)
+	 */
+	public AbstractWareDocumentEntity load(WareDocumentID id) throws WareDocumentLoadException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.lifecycle.WareDocumentLoader#load(org.ghcc.toft.ware.core.design.interfaces.mop.caas.waredocument.define.WareDocumentPathInfo, org.ghcc.toft.ware.core.design.interfaces.cop.define.ID)
+	 */
+	public AbstractWareDocumentEntity load(WareDocumentPathInfo pathInfo, WareDocumentID id)
+			throws WareDocumentLoadException {
+		AbstractWareDocumentEntity wareDocumentEntity = null;
+		URL url = pathInfo.getWareDocumentPathURL();
+		try {
+			URL realURL = new URL(url, id.getID());
+			SAXReader saxReader = new SAXReader();
+			Document document = null;
+			File wareDocumentFile = new File(realURL.getPath());
+			if (wareDocumentFile.exists()) {
+				document = saxReader.read(wareDocumentFile);
+			} else {
+				document = saxReader.read(new InputStreamReader(url.openStream()));
+			}
+			wareDocumentEntity = new AbstractWareDocumentEntity(document);
+			
+		} catch (Exception e) {
+			WareDocumentLoadException wareDocumentLoadException = new WareDocumentLoadException();
+			wareDocumentLoadException.setJavaException(e);
+			throw wareDocumentLoadException;
+		}
+		
+		return wareDocumentEntity;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		URL parent = new URL("http://i:9999/");
+		URL url = new URL(parent, "text.ware.xml");
+		System.out.println(url);
+	}
+}
