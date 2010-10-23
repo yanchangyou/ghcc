2834a2f4d2978c80ed29d9424f642fbc5552e3ea
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-10-23 13:20:13 +0000

    yanchangyou->org.ghcc.toft-ware.core.design-surpass-0.1-1-1 0，确定结构优先的方式:如果在0.1版, 如果结构调整就废弃, 使用新版; 使用左小数, 右整数规范, 这样减少过多分配版本, 如果左边达到整数, 右边就按小数分开 1, 新建项目 2, 使用cop概念 3，把原来的saas放到concept包下 4，调整mop包结构,附件concept包用于区分

---
+++ groups/org/ghcc/toft/toft-0/toft-0.1/products/ware/core/design/design-1/projects/surpass/surpass-1/org.ghcc.toft-ware.core.design-surpass-0.1-1-1/java-code/org/ghcc/toft/ware/core/design/interfaces/mop/caas/output/OutputEntity.java
@@ -0,0 +1,23 @@
+/*
+ * CopyCenter 2010 GHCC.ORG, all rights freed.
+ */
+
+package org.ghcc.toft.ware.core.design.interfaces.mop.caas.output;
+
+import org.ghcc.toft.ware.core.design.interfaces.concept.caas.Output;
+import org.ghcc.toft.ware.core.design.interfaces.cop.Entity;
+
+
+/**
+ * OutputEntity
+ * 
+ * @author code machine
+ * @author yanchangyou
+ * @date 2010-10-23 21:10:15
+ * @version 0.1-1-1
+ *
+ */
+
+
+public interface OutputEntity extends Output, Entity {
+}
---
+++ groups/org/ghcc/toft/toft-0/toft-0.1/products/ware/core/design/design-1/projects/surpass/surpass-1/org.ghcc.toft-ware.core.design-surpass-0.1-1-1/java-code/org/ghcc/toft/ware/core/design/interfaces/mop/caas/saas/lifecycle/SAASRecycler.java
@@ -0,0 +1,23 @@
+/*
+ * CopyCenter 2010 GHCC.ORG, all rights freed.
+ */
+
+package org.ghcc.toft.ware.core.design.interfaces.mop.caas.saas.lifecycle;
+
+import org.ghcc.toft.ware.core.design.interfaces.concept.caas.SAAS;
+import org.ghcc.toft.ware.core.design.interfaces.cop.lifecycle.Recycler;
+
+
+/**
+ * SAASRecycler
+ * 
+ * @author code machine
+ * @author yanchangyou
+ * @date 2010-10-23 21:10:15
+ * @version 0.1-1-1
+ *
+ */
+
+
+public interface SAASRecycler extends SAAS, Recycler {
+}
