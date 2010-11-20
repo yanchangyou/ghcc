5a22c199d790b2a9fc1aeccc4f67647c050025ae
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-11-20 17:32:26 +0000

    yanchangyou->org.ghcc.demo.ware.helloworld-0.5.0.1-run 1, 添加helloworld程序实现

---
+++ GHCC/GHCC-1/org/ghcc/demo/ware/helloworld/0/5/0/1/run/org.ghcc.demo.ware.helloworld-0.5.0.1-run/java-code/org/ghcc/demo/ware/helloworld/HelloworldWare.java
@@ -0,0 +1,16 @@
+package org.ghcc.demo.ware.helloworld;
+
+import java.net.URL;
+
+import org.ghcc.toft.ware.vendor.ether.impl.defaults.mop.caas.ware.DefaultEtherWareEntity;
+
+public class HelloworldWare extends DefaultEtherWareEntity {
+
+	/**
+	 * @return
+	 */
+	public URL[] configClassPath() {
+		return new URL[0];
+	}
+	
+}
