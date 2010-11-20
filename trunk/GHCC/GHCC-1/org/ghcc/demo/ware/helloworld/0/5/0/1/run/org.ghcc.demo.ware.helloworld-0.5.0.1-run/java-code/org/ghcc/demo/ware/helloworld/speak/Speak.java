88602327ff8856c197433a8a89d093b436a5df80
yanchangyou@gmail.com <yanchangyou@gmail.com@1b3c01a9-0980-7ae1-184d-a1ecbe3d4eca>
2010-11-20 17:32:26 +0000

    yanchangyou->org.ghcc.demo.ware.helloworld-0.5.0.1-run 1, 添加helloworld程序实现

---
+++ trunk/GHCC/GHCC-1/org/ghcc/demo/ware/helloworld/0/5/0/1/run/org.ghcc.demo.ware.helloworld-0.5.0.1-run/java-code/org/ghcc/demo/ware/helloworld/speak/Speak.java
@@ -0,0 +1,30 @@
+/*
+ * CopyCenter 2010 GHCC.ORG, all rights freed.
+ */
+
+package org.ghcc.demo.ware.helloworld.speak;
+
+import org.ghcc.toft.ware.norm.interfaces.mop.caas.function.exception.FunctionDriveException;
+import org.ghcc.toft.ware.vendor.ether.design.interfaces.mop.caas.function.define.EtherFunctionContext;
+import org.ghcc.toft.ware.vendor.ether.impl.abstracts.mop.caas.function.AbstractEtherFunctionEntity;
+
+/**
+ * Speak
+ * 
+ * @author yanchangyou
+ * @date 2010-11-20 下午10:42:00
+ * @version 0.1-1-1
+ *
+ */
+
+public class Speak extends AbstractEtherFunctionEntity {
+
+	/**
+	 * @param context
+	 * @throws FunctionDriveException
+	 */
+	public void drive(EtherFunctionContext context) throws FunctionDriveException {
+		System.out.println(this.getFunctionElement().getText());
+	}
+
+}
